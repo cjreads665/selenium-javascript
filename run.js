74f35dabdd6dffb0075ef6e9fa476c01136f6e9e
cjreads665 <shahid.alam57@gmail.com>
2023-03-03 21:35:14 +0545

    completed one form

--- run.js
+++ run.js
@@ -20,23 +20,15 @@ describe('CloudQA Tests', function() {
      done();
   });
 
-//   it('Fill the first form', async function() {
-//     try {
-//       await driver.findElement(By.id('fname')).sendKeys("Shahid");
-//       await driver.findElement(By.id('lname')).sendKeys("Alam");
-//       await driver.findElement(By.id('dob')).sendKeys("2001-08-02");
-//       await driver.findElement(By.id('Agree')).click(); // automationtestform
-//       await driver.findElement(By.id('automationtestform')).click();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
-
   it('Fill the form without iframe', async function() {
 
     try {
         const h1Elements = await driver.findElements(By.css('h1'));
         let iframeContainer;
+        /**
+         * go through each h1 elements in the page and find the one with
+         * 'IFrame without ID' text and then select the next iframe tag after it
+         */
         for (let h1 of h1Elements) {
           const text = await h1.getText();
           if (text.includes('IFrame without ID')) {
@@ -46,16 +38,55 @@ describe('CloudQA Tests', function() {
           }
         }
         
-        // switch to the iframe context
+        /*switch to the iframe context
+          use this to switch to default context driver.switchTo().defaultContent().      
+          the frame() exists for iframe elements only
+        */
         await driver.switchTo().frame(iframeContainer);
         
-        // find the element with id "fname" inside the iframe
-        const fnameInput = await driver.findElement(By.id("fname"));
+      await driver.findElement(By.id('fname')).sendKeys("Shahid");
+      await driver.findElement(By.id('lname')).sendKeys("Alam");
+      await driver.findElement(By.id('dob')).sendKeys("2001-08-02");
+      await driver.findElement(By.id('Agree')).click(); // automationtestform
+      await driver.findElement(By.id('automationtestform')).click();
+
+      await new Promise(resolve => setTimeout(resolve, 10000)); //for debugging
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  it('Fill the form with IFrame without ID', async function() {
+
+    try {
+        const h1Elements = await driver.findElements(By.css('h1'));
+        let iframeContainer;
+        /**
+         * go through each h1 elements in the page and find the one with
+         * 'IFrame without ID' text and then select the next iframe tag after it
+         */
+        for (let h1 of h1Elements) {
+          const text = await h1.getText();
+          if (text.includes('IFrame without ID')) {
+            //select the following iframe element after the h1 with text - IFrame without ID
+            iframeContainer = await h1.findElement(By.xpath('following-sibling::iframe'));
+            break;
+          }
+        }
         
-        // perform an action on the element, e.g. sending keys
-        await fnameInput.sendKeys("John");
+        /*switch to the iframe context
+          use this to switch to default context driver.switchTo().defaultContent().      
+          the frame() exists for iframe elements only
+        */
+        await driver.switchTo().frame(iframeContainer);
         
-      setTimeout(() => {}, 5000);
+      await driver.findElement(By.id('fname')).sendKeys("Shahid");
+      await driver.findElement(By.id('lname')).sendKeys("Alam");
+      await driver.findElement(By.id('dob')).sendKeys("2001-08-02");
+      await driver.findElement(By.id('Agree')).click(); // automationtestform
+      await driver.findElement(By.id('automationtestform')).click();
+      await driver.switchTo().defaultContent();
+      await new Promise(resolve => setTimeout(resolve, 10000)); //for debugging
     } catch (error) {
       console.log(error);
     }
